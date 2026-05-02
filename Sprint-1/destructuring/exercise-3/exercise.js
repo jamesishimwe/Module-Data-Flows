9d8336e09b043fa37c7e5ade575dbad1780430c9
jamesishimwe <james123ishimwe@gmail.com>
2026-05-02 22:55:46 +0200

    Implement function to get total

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,18 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function receipt(order)
+{
+let total = 0;
+
+  console.log("QTY".padEnd(7) + "ITEM".padEnd(20) + "TOTAL".padStart(5));
+  for(let obj of order)  {
+  let{itemName,quantity,unitPricePence} = obj;
+  total = quantity * unitPricePence;
+  let row = quantity.toString().padEnd(7) + itemName.padEnd(20) + total.toString().padStart(5);
+  console.log(row);
+}
+console.log("\nTotal: " + total.toString());
+}
+receipt(order);
