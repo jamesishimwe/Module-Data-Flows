d2dcde1a610e8327a3b7b7261836129b59bea008
jamesishimwe <james123ishimwe@gmail.com>
2026-05-18 22:42:04 +0200

    Add button to display image

--- fetch/programmer-humour/index.js
+++ fetch/programmer-humour/index.js
@@ -1,14 +1,26 @@
-let data;
-async function fetchData() {
-    data = await fetch('https://xkcd.now.sh/?comic=latest')
+
+async function app() {
+   let data = await fetch('https://xkcd.now.sh/?comic=latest')
     .then(response => {
         if(!response.ok) throw new Error("Response error");
         return response.json();
     })
     .catch(error=> { throw new error('Response error')});
     console.log(data);
+const generateImageButton = document.createElement("button");
+const divOfImage = document.createElement("div");
 image = document.createElement("img");
 image.src = `${data.img}`;
-document.body.append(image);
+document.body.append(divOfImage);
+document.body.append(generateImageButton);
+generateImageButton.innerText = "Display";
+generateImageButton.addEventListener("click",generate);
+divOfImage.style.height = "300px";
+
+function generate(){
+    image.style.height = "100%";
+    divOfImage.append(image);
 }
-window.onload = fetchData();
+}
+
+window.onload = app;
