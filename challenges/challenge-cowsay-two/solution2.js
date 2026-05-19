f9513caa0fe77dee1e8462f48202a37f9780726b
jamesishimwe <james123ishimwe@gmail.com>
2026-05-19 15:55:37 +0200

    Added the cow  function

--- challenges/challenge-cowsay-two/solution2.js
+++ challenges/challenge-cowsay-two/solution2.js
@@ -9,10 +9,23 @@
 // 2. Make supplies for our speech bubble
 
 // 3. Make a cow that takes a string
+let topLine = '_';
+let bottomLine = '-';
+let saying = '';
 
 const cow = (saying) => {
     // how did you make the cow before?
+    saying = ' '+saying+' ';
+    for(let i=1;i<saying.length;i++)
+{
+    topLine += "_"; 
+    bottomLine+='-';
 }
+
+ let cow =` ${topLine}\n<${saying}>\n ${bottomLine}\n         \\    ^__^\n          \\   (oo)\\_______\n              (__)\\       )\\/\\ \n                 ||----w |\n                 ||     ||`;
+    console.log(cow);
+}
+
 
 // 4. Use readline to get a string from the terminal 
 // (with a prompt so it's clearer what we want)
