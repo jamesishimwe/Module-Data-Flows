23e681e3531dca219293eed91c10916f9a1c4f60
jamesishimwe <james123ishimwe@gmail.com>
2026-05-20 13:32:03 +0200

    Style the image by adding the border and width and height

--- challenges/dog-photo-gallery/index.js
+++ challenges/dog-photo-gallery/index.js
@@ -3,11 +3,13 @@ async function generate() {
     .then(response=>response.json())
     .catch(error=>{throw new Error("Error")});
    img.src = image.message;
+ 
 }
  const btn = document.getElementById("btn");
     const list  = document.getElementById("list");
     const img  = document.createElement("img");
+      img.style.border = "2px solid black";
     img.style.height = "100%";
-    
+    img.style.width ="600px";
     list.append(img);
     btn.addEventListener("click",generate);
