346f963b66b7e414f6a75e208e4c16b8cd8314bd
jamesishimwe <james123ishimwe@gmail.com>
2026-05-20 16:13:43 +0200

    Add button to display images according to description of the weather

--- challenges/challenge-weather-app/assets/index.js
+++ challenges/challenge-weather-app/assets/index.js
@@ -8,6 +8,31 @@ async function getWeather()
     let images = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=rFjekzFfQzet2DcZuW1kKXe8n9DHZHkp4zy2xGSbV3o`)
     .then(response=>response.json())
     .catch(error=>{throw new Error("Error")});
-    console.log(images);
+    console.log(images.results);
+    const links = images.results;
+const figure = document.getElementById("photo");
+const button = document.createElement("button");
+document.querySelector(".info").append(button);
+button.innerText = "Next";
+button.style.position = "absolute";
+button.style.left = "50%";
+button.style.bottom = "40px";
+button.style.borderRadius = "10px";
+const imageFigure = document.createElement("img");
+figure.append(imageFigure);
+imageFigure.style.height = "100%";
+imageFigure.style.width = "100%";
+let img = 0;
+imageFigure.src= links[0].urls.full;
+button.addEventListener('click', ()=>{
+     img += 1;
+    if(img === links.length) img =0;
+    console.log(imageFigure.src);
+    imageFigure.src = links[img].urls.full;
+
+
+});
+
+
 }
 getWeather();
