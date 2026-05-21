e628b4285998d0b4faf545b70d33aee7f07edef5
jamesishimwe <james123ishimwe@gmail.com>
2026-05-21 16:16:33 +0200

    Fix bugs

--- challenges/unit-testing/katas-tdd/calculator/calculator.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.js
@@ -1,26 +1,33 @@
 function calculator(numbers)
 {
-// if(numbers === "") return 0;
-// else if(numbers.length === 1 && typeof(parseInt(numbers))==="number") return(parseInt(numbers));
-// else if(numbers.length === 3){
-// let number = parseInt(numbers.split(","));
-// let secondNumber = parseInt(numbers.split(",")[1]);
-// return number + secondNumber;
-// }
+ if(numbers === "") return 0;
+ else if(numbers.length === 1 && numbersYes(numbers)) return(parseInt(numbers));
+ else if(numbers.length === 3 && numbersYes(numbers)){
+ let number = parseInt(numbers.split(","));
+ let secondNumber = parseInt(numbers.split(",")[1]);
+ return number + secondNumber;
+ }
+ else if(numbers.length>3 && numbersYes(numbers)){
+    let arr = numbers.split(",");
+    let total = 0;
+    for(let i=0;i<numbers.length;i+=2)
+    {
+        total += parseInt(numbers[i]);
+    }
+    return total;
+ }
+ 
 function numbersYes(numbers)
 {   let yes = true;
     let arr = [];
 for(let i=0;i<numbers.length;i+=2)
     {  
        arr.push(parseInt(numbers[i]));
-       console.log(arr);
     }
     for(x of arr) if(Number.isNaN(x)) yes = false;
-    console.log(yes);
    return yes;
 
 }
-numbersYes(numbers);
 }
-calculator("3,,1,4,5,5,6,5,4,4,6,7");
+
 module.exports = calculator
