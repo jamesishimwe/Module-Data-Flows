e6db6275f28c49c18fe2901fc2280da7be78ed11
jamesishimwe <james123ishimwe@gmail.com>
2026-05-21 18:59:21 +0200

    Add test for negative numbers

--- challenges/unit-testing/katas-tdd/calculator/calculator.test.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.test.js
@@ -4,8 +4,8 @@ test("Returns zero for empty string",()=>{
 
 });
 test("Returns a number if passed without another",()=>{
-    expect(calculator("-5")).toEqual(-5);
-})
+    expect(calculator("5")).toEqual(5);
+});
 
 test('Returns sum of two numbers',()=>{
     expect(calculator('3,6')).toEqual(9);
@@ -16,4 +16,8 @@ test('Returns sum of two numbers',()=>{
 
 test('Ignore big numbers',()=>{
 expect(calculator('2,4,1,2,4,5,6,1001')).toEqual(24);
+});
+
+test('Throw an error for negative numbers',()=>{
+    expect(()=>{calculator('1,2,4,5,-8,-10')}).toThrow("negatives not allowed: -8,-10");
 });
