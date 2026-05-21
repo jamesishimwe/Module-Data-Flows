25a795c49042707560d7e085d678bea124a63466
jamesishimwe <james123ishimwe@gmail.com>
2026-05-21 14:25:53 +0200

    Implement the test for single numbers

--- challenges/unit-testing/katas-tdd/calculator/calculator.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.js
@@ -1,7 +1,7 @@
 function calculator(numbers)
 {
 if(numbers === "") return 0;
-//else if();
+else if(numbers.length === 1 && typeof(parseInt(numbers))===Number) return parseInt(numbers);
 }
 
 module.exports = calculator
