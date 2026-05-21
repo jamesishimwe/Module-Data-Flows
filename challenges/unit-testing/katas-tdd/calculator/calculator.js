6c9353439bffd694a4d68ceacc0d81390c1e9bd8
jamesishimwe <james123ishimwe@gmail.com>
2026-05-21 17:38:31 +0200

    Refactor code

--- challenges/unit-testing/katas-tdd/calculator/calculator.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.js
@@ -1,34 +1,39 @@
 function calculator(numbers)
 {
  if(numbers === "") return 0;
- else if(numbers.length === 1 && numbersYes(numbers)) return(parseInt(numbers));
+ else if(numbers.length === 1 && numbersYes(numbers)) return(Number(numbers));
  else if(numbers.length === 3 && numbersYes(numbers)){
- let number = parseInt(numbers.split(","));
- let secondNumber = parseInt(numbers.split(",")[1]);
+ let number = Number(numbers.split(","));
+ let secondNumber = Number(numbers.split(",")[1]);
  return number + secondNumber;
  }
  else if(numbers.length>3 && numbersYes(numbers)){
     let arr = numbers.split(",");
     let total = 0;
     for(let i=0;i<arr.length;i++)
-    {   let number = parseInt(arr[i]);  
+    {   let number = Number(arr[i]);  
         if(number >1000) continue;
         total += number;
     }
     return total;
     
 }
+
+}
 function numbersYes(numbers)
 {   let yes = true;
-    let arr = [];
-for(let i=0;i<numbers.length;i+=2)
-    {  
-       arr.push(parseInt(numbers[i]));
+    let arr = numbers.split(",");
+    console.log(arr);
+    for(let i =0; i<arr.length;i++)
+    {
+        arr[i] = Number(arr[i]);
+        console.log(arr[i]);
+        if(Number.isNaN(arr[i])) yes = false;
     }
-    for(x of arr) if(Number.isNaN(x)) yes = false;
+    console.log(arr);
+    console.log(yes);
    return yes;
 
 }
-}
-calculator("1,2,3,4,5,6,7000");
+
 module.exports = calculator
