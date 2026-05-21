cb7fba53306b8c3f38504958d9033b28aee19042
jamesishimwe <james123ishimwe@gmail.com>
2026-05-21 15:04:37 +0200

    Implement the test for two numbers

--- challenges/unit-testing/katas-tdd/calculator/calculator.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.js
@@ -2,7 +2,11 @@ function calculator(numbers)
 {
 if(numbers === "") return 0;
 else if(numbers.length === 1 && typeof(parseInt(numbers))==="number") return(parseInt(numbers));
-
+else if(numbers.length === 3){
+let number = parseInt(numbers.split(","));
+let secondNumber = parseInt(numbers.split(",")[1]);
+return number + secondNumber;
+}
 }
 
 module.exports = calculator
