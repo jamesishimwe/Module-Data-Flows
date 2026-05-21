8c6e0b9302b04d0e1fcbc871fa566c9e4a2296f8
jamesishimwe <james123ishimwe@gmail.com>
2026-05-21 16:07:34 +0200

    Create function to check if arg is numbers and in order

--- challenges/unit-testing/katas-tdd/calculator/calculator.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.js
@@ -1,12 +1,26 @@
 function calculator(numbers)
 {
-if(numbers === "") return 0;
-else if(numbers.length === 1 && typeof(parseInt(numbers))==="number") return(parseInt(numbers));
-else if(numbers.length === 3){
-let number = parseInt(numbers.split(","));
-let secondNumber = parseInt(numbers.split(",")[1]);
-return number + secondNumber;
-}
-}
+// if(numbers === "") return 0;
+// else if(numbers.length === 1 && typeof(parseInt(numbers))==="number") return(parseInt(numbers));
+// else if(numbers.length === 3){
+// let number = parseInt(numbers.split(","));
+// let secondNumber = parseInt(numbers.split(",")[1]);
+// return number + secondNumber;
+// }
+function numbersYes(numbers)
+{   let yes = true;
+    let arr = [];
+for(let i=0;i<numbers.length;i+=2)
+    {  
+       arr.push(parseInt(numbers[i]));
+       console.log(arr);
+    }
+    for(x of arr) if(Number.isNaN(x)) yes = false;
+    console.log(yes);
+   return yes;
 
+}
+numbersYes(numbers);
+}
+calculator("3,,1,4,5,5,6,5,4,4,6,7");
 module.exports = calculator
