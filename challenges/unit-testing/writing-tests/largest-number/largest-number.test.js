892a085a1310bd75d5a119582767d7201bdec248
jamesishimwe <james123ishimwe@gmail.com>
2026-05-22 02:18:23 +0200

    Add test for the largest number

--- challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
+++ challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
@@ -1,9 +1,10 @@
 let getLargestNumber = require("./largest-number");
 
 test("returns largest number in array", function () {
-  // Arrange
-  // Act
-  // Assert
+  let input = [3, 21, 88, 4, 36];
+ let expected = 88;
+
+  expect(getLargestNumber(input)).toEqual(expected);
 });
 
 // example
