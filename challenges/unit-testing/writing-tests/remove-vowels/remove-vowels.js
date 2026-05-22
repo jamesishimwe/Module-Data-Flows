712db569b34c836d0856c4b2f5ac47b1ab463792
jamesishimwe <james123ishimwe@gmail.com>
2026-05-22 02:35:08 +0200

    Add the capital letter removing

--- challenges/unit-testing/writing-tests/remove-vowels/remove-vowels.js
+++ challenges/unit-testing/writing-tests/remove-vowels/remove-vowels.js
@@ -9,7 +9,12 @@ function removeVowels(word) {
       character !== "o" &&
       character !== "i" &&
       character !== "e" &&
-      character !== "u"
+      character !== "u" &&
+      character !== "A" &&
+      character !== "E" &&
+      character !== "I" &&
+      character !== "U" &&
+      character !== "O"
     ) {
       result.push(character);
     }
