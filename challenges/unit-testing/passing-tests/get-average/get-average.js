16a6f8b5ed9672c133540b63353471c9e5a43992
jamesishimwe <james123ishimwe@gmail.com>
2026-05-22 02:12:57 +0200

    Implement function get-average

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,12 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
-
+function average(numbers) {
+    let average = 0;
+    numbers = numbers.filter( (num) =>typeof num==="number");
+    console.log(numbers);
+    for(let num of numbers) average+=num;
+    return average/numbers.length;
+}
+average([4, "-", 8, 11, "hello", "57", 0, 2]);
 module.exports = average;
